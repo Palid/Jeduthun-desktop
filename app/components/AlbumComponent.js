ee3fa88f420b2be3c990f881fd52d89ee46874dd
Dawid Gorczyca <Dawid Gorczyca>
2017-04-16 17:17:08 +0200

    pagination list styles

--- app/components/AlbumComponent.js
+++ app/components/AlbumComponent.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import styles from './AlbumComponent.css'
 import { truncateString } from '../utils/componentUtils'
+import imagePlus from '../media/add-to-playlist.svg'
 
 const AlbumComponent = props =>
   <div className={styles.album}>
     <img src={props.album.cover.url} alt="cover"/>
     <h2>{truncateString.apply(props.album.title, [28, false])}</h2>
     <div className={styles.albumButton}>
-      <button onClick={() => props.addToPlaylistAction(props.album)}>Add to playlist</button>
+      <button onClick={() => props.addToPlaylistAction(props.album)}>
+        <svg width="50px" height="50px" viewBox="0 0 50 50">
+          <polygon fill-rule="evenodd" clip-rule="evenodd" fill="#888131" points="33,17 33,0 17,0 17,17 0,17 0,33 17,33 17,50 33,50 
+		33,33 50,33 50,17 	"/>
+        </svg>
+      </button>
     </div>
   </div>
 
--- app/components/PaginationComponent.js
+++ app/components/PaginationComponent.js
@@ -1,10 +1,23 @@
 // @flow
 import React from 'react'
+import styles from './PaginationComponent.css'
+import imageArrowNext from '../media/arrow-next.svg'
+import imageArrowPrev from '../media/arrow-prev.svg'
 
 const PaginationComponent = props =>
-  <div className="pagination">
-    <button onClick={props.handlePageLoadPrev}>PrevPage</button>
-    <button onClick={props.handlePageLoadNext}>NextPage</button>
+  <div className={styles.pagination}>
+    <button onClick={props.handlePageLoadPrev}>
+      <svg width="11.314px" height="22.627px" viewBox="0 0 11.314 22.627">
+        <polygon fill="#E4E4E4" points="0,11.314 11.314,0 11.314,22.627 	"/>
+      </svg>
+      PREV
+    </button>
+    <button onClick={props.handlePageLoadNext}>
+      NEXT
+      <svg width="11.314px" height="22.627px" viewBox="0 0 11.314 22.627" >
+        <polygon fill-rule="evenodd" clip-rule="evenodd" fill="#E4E4E4" points="11.314,11.313 0,22.627 0,0 	"/>
+      </svg>
+    </button>
   </div>
 
 export default PaginationComponent
